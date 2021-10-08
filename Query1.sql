d791b7e1997b33498a1d7857e1a590be920258d4
Andrey-Berezin <andreyberezin55@gmail.com>
2021-10-08 21:11:28 +0600

    Добавлена ER-модель и SQL-запросы к БД

---
+++ Query1.sql
@@ -0,0 +1,2 @@
+use salary_of_employees;
+select * from сотрудник where сотрудник.Отдел_Наименование = 'Тестирование';
---
+++ Query3.sql
@@ -0,0 +1,2 @@
+use salary_of_employees;
+select * from сотрудник where сотрудник.Должность_Наименование = 'Аналитик';
